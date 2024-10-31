c36b3ecd28f2ef8ace7a8615df017dc935181eff
jj8127 <jj81271000@gmail.com>
2024-10-31 13:08:16 +0900

    first commit

--- app/server.py
+++ app/server.py
@@ -5,9 +5,6 @@ from typing import List, Union
 from langserve.pydantic_v1 import BaseModel, Field
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langserve import add_routes
-from chain import chain
-from chat import chain as chat_chain
-from translator import chain as EN_TO_KO_chain
 from llm import llm as model
 
 
@@ -29,8 +26,6 @@ async def redirect_root_to_docs():
     return RedirectResponse("/llm/playground")
 
 
-add_routes(app, chain, path="/prompt")
-
 
 class InputChat(BaseModel):
     """Input for the chat endpoint."""
@@ -40,17 +35,6 @@ class InputChat(BaseModel):
         description="The chat messages representing the current conversation.",
     )
 
-
-add_routes(
-    app,
-    chat_chain.with_types(input_type=InputChat),
-    path="/chat",
-    enable_feedback_endpoint=True,
-    enable_public_trace_link_endpoint=True,
-    playground_type="chat",
-)
-
-add_routes(app, EN_TO_KO_chain, path="/translate")
 
 add_routes(app, model, path="/llm")
 
