e7e1e12a9580bb909e0478a7c4ffe55b5022b9c4
jj8127 <jj81271000@gmail.com>
2025-01-09 21:13:41 +0900

    revise_model

--- app/chat.py
+++ app/chat.py
@@ -3,8 +3,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 # LangChain이 지원하는 다른 채팅 모델을 사용합니다. 여기서는 Ollama를 사용합니다.
-llm = ChatOllama(model="ollama 모델 이름을 넣으세요")
-llm = ChatOllama(model="DUchatbot:latest")
+llm = ChatOllama(model="DUCChatbot5ep:latest")
 
 
 # Prompt 설정
